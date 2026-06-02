742a85186d1680fe3f0dd916bfb188a005cd2eb4
razeem07 <rrazeem07@gmail.com>
2026-06-02 10:51:53 +0530

    deleted wrong directory

--- core/products/models.py
+++ core/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
